446d0924dafe7b5d00e379a98958cfd4cc6d65d8
Alexandru Patranescu <alexandru.patranescu@payu.ro>
2013-05-21 14:55:51 +0300

    finished backend cache

--- tests/Layeredcache/Tests/CacheBackendTest.php
+++ tests/Layeredcache/Tests/CacheBackendTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace LayeredCache\Tests;
 
+use LayeredCache\Backend\TaggableCache;
+
 abstract class CacheBackendTest extends LayeredCacheTest
 {
     /**
@@ -11,13 +13,14 @@ abstract class CacheBackendTest extends LayeredCacheTest
     public function testPutGetRemove()
     {
         $cacheBackend = $this->getCacheBackend();
-        
+	    
         $this->assertTrue($cacheBackend->put('test1', 'common test text'));
         $this->assertTrue($cacheBackend->contains('test1'));
         $this->assertEquals('common test text', $cacheBackend->get('test1'));
         $this->assertTrue($cacheBackend->remove('test1'));
         $this->assertFalse($cacheBackend->contains('test1'));
         $this->assertNull($cacheBackend->get('test1'));
+	    $this->assertFalse($cacheBackend->remove('test1'));
     }
     
     public function testPutGetWithExpire()
@@ -55,6 +58,31 @@ abstract class CacheBackendTest extends LayeredCacheTest
         $this->assertTrue($cacheBackend->decrement('test3'));
         $cacheBackend->decrement('test3');
         $this->assertEquals(3, $cacheBackend->get('test3'));
+	    $cacheBackend->remove('test3');
+	    $this->assertFalse($cacheBackend->increment('test3'));
+	    $this->assertFalse($cacheBackend->decrement('test3'));
     }
+	
+	public function testSetGetTags()
+	{
+		$cacheBackend = $this->getCacheBackend();
+		if (!$cacheBackend instanceof TaggableCache) {
+			return;
+		}
+
+		$this->assertTrue($cacheBackend->putWithTags('test4', 'tags string cache', array('tag1', 'tag2')));
+		$this->assertContains('tag1', $cacheBackend->getTags('test4'));
+		$this->assertContains('tag2', $cacheBackend->getTags('test4'));
+		$cacheBackend->putWithTags('test5', 'tags string cache', array('tag1', 'tag3'));
+		$this->assertContains('test4', $cacheBackend->getIdsByTags(array('tag1', 'tag2')));
+		$this->assertEmpty($cacheBackend->getIdsByTags(array('tag2', 'tag3')));
+		$this->assertCount(2, $cacheBackend->getIdsByAnyTags(array('tag1', 'tag2', 'tag3')));
+		$this->assertTrue($cacheBackend->clearByTags(array('tag1', 'tag2', 'tag3')));
+		$this->assertCount(2, $cacheBackend->getIdsByAnyTags(array('tag1', 'tag2', 'tag3')));
+		$this->assertTrue($cacheBackend->clearByTags(array('tag1', 'tag3')));
+		$this->assertCount(1, $cacheBackend->getIdsByAnyTags(array('tag1', 'tag2', 'tag3')));
+		$this->assertTrue($cacheBackend->clearByAnyTags(array('tag1', 'tag3')));
+		$this->assertCount(0, $cacheBackend->getIdsByAnyTags(array('tag1', 'tag2', 'tag3')));
+	}
     
 }
